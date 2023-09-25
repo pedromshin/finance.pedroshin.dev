cff7ab1f6fd389bcff6cd3f9868f8a0e80e0348e
Pedro Shin <pedromaschio.shin@gmail.com>
2023-09-25 10:42:25 -0300

    fix websocket event emission

--- application.py
+++ application.py
@@ -15,34 +15,33 @@ thread = None
 thread_lock = Lock()
 
 url = 'https://api.coinbase.com/v2/prices/btc-usd/spot'
+response_event = "response_to_frontend"
 
 
 def background_thread():
+    print("QWEQWE")
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
+        print(count)
         socketio.sleep(3)
         count += 1
         price = ((requests.get(url)).json())['data']['amount']
-        socketio.emit('price_update',
-                      {'data': 'Bitcoin current price (USD): ' + price, 'count': count}, namespace='/')
-
-
-@application.route('/')
-def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+        socketio.emit(response_event,
+                      {'data': 'Bitcoin current price (USD): ' + price, 'count': count})
 
 
 @socketio.event
 def my_event(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+    receive_count = "receive_count"
+    session[receive_count] = session.get(receive_count, 0) + 1
+    emit(response_event,
+         {'data': message['data'], 'count': session[receive_count]})
 
 
 @socketio.on('message_from_frontend')
 def handle_message_from_frontend(message):
-    emit('response_to_frontend', {'data': 'Server received: ' + message})
+    emit(response_event, {'data': 'Server received: ' + message})
 
 
 @socketio.on('connect')

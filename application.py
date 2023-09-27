811b577174ef3ff1f75c014c3f6305b0aea4a969
Pedro Shin <pedromaschio.shin@gmail.com>
2023-09-26 23:17:25 -0300

    adds connection to pgsql databate

--- application.py
+++ application.py
@@ -3,9 +3,23 @@ from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
 import requests
 
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on installed packages.
+import psycopg2
+
+from decouple import config
+
+# Database configuration
+# db_config = {
+#     "dbname": config('DB_NAME'),
+#     "user": config('DB_USER'),
+#     "password": config('DB_PASSWORD'),
+#     "host": config('DB_HOST'),
+#     "port": config('DB_PORT'),
+# }
+
+# conn = psycopg2.connect(**db_config)
+
+# print("Database connected successfully", conn)
+
 async_mode = None
 
 application = Flask(__name__)
@@ -22,10 +36,15 @@ def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
-        print(count)
-        socketio.sleep(3)
+        socketio.sleep(1)
         count += 1
         price = ((requests.get(url)).json())['data']['amount']
+
+        # with conn.cursor() as cur:
+        #     cur.execute("INSERT INTO btc_prices (price, timestamp) VALUES (%s, NOW())", (price,))
+        #     conn.commit()
+
+        print(price)
         socketio.emit(response_event,
                       {'price': price, 'count': count, 'currency': 'USD'})
 
@@ -38,11 +57,6 @@ def my_event(message):
          {'data': message['data'], 'count': session[receive_count]})
 
 
-@socketio.on('message_from_frontend')
-def handle_message_from_frontend(message):
-    emit(response_event, {'data': 'Server received: ' + message})
-
-
 @socketio.on('connect')
 def connect():
     global thread
@@ -53,4 +67,7 @@ def connect():
 
 
 if __name__ == '__main__':
-    socketio.run(application, host='0.0.0.0', port=5000, debug=True)
+    try:
+        socketio.run(application, host='0.0.0.0', port=5000, debug=True)
+    finally:
+        conn.close()
